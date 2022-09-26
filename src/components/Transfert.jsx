1f6ee8944cc5175214867ebc6f15fe9f97881a1f
thorxx19 <froidefond.olivier@outlook.fr>
2022-09-26 23:51:47 +0200

    modif toast

--- src/components/Transfert.jsx
+++ src/components/Transfert.jsx
@@ -8,24 +8,23 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
 import Toast from 'react-bootstrap/Toast';
 import ToastContainer from 'react-bootstrap/ToastContainer';
+import Pagination  from "./Pagination";
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import logo from '../assets/logo_Pay.png'
 
 
 class Transfert extends React.Component{
     constructor(props){
         super(props)
-        this.state = {transfert: [], connect : [], value : 0, valueCount : 0, id : 0, show1 : false, show2 : false} 
+        this.state = {connect : [], value : 0, valueCount : 0, id : 0, show1 : false, show2 : false} 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this); 
         this.handleChangeCount = this.handleChangeCount.bind(this);
-        
-     
     }
     handleChange(event) {
-        console.log(event.target.value)
         this.setState({value: event.target.value});
       }
     handleChangeCount(event){
-        console.log(event.target.value)
         this.setState({valueCount: event.target.value})
     }
     hideComponent(name){
@@ -34,27 +33,30 @@ class Transfert extends React.Component{
                 this.setState({show1: !this.state.show1})
                 break;
             case "show2":
-                this.setState({show2: !this.state.show2})    
+                this.setState({show2: !this.state.show2})
+                break;    
             default:
                 break;
         }
     }
+    
     handleSubmit(event) {
-        if (this.state.value !== 0 && this.state.valueCount !== 0) {
-            Connection.postTransfert(this.state.id,this.state.value,this.state.valueCount).then((respons)=>{
+        const {valueCount, value, id} = this.state
+        if (value !== 0 && valueCount > 0) {
+            Connection.postTransfert(id, value, valueCount).then((respons)=>{
                 respons === "no save" ? this.setState({show2: !this.state.show2}) : this.setState({show1: !this.state.show1})
-                respons === "no save" ? console.log("no save") : window.location.reload(false)
             }) 
+            setTimeout(() => {
+                window.location.reload(false)
+            }, 3000);
         } else {
             this.setState({show2: !this.state.show2})
+            
         }
         event.preventDefault();
     }
 
     componentDidMount(){
-        Connection.getAllTransfer().then((respons)=>{
-            respons === 0 ? this.setState({transfert: []}) : this.setState({transfert: respons})
-        })
         Connection.getConnectById().then((responsConnect)=>{
             responsConnect === 0 ? this.setState({connect: []}) : this.setState({connect : responsConnect.data})
             responsConnect === 0 ? this.setState({id: 0}) : this.setState({id : responsConnect.data[0].idUn.id})
@@ -64,15 +66,17 @@ class Transfert extends React.Component{
     render (){
         const {show1, valueCount, value, show2} = this.state
         return (
-            <Container>
-                
-                <p className="text-start">Home  /  Transfer</p>
-                <Row className="row justify-content-between my-3">
-                    <Col xs={6}>
+            <Container fluid='md' className="justify-content-md-center">
+                <Breadcrumb>
+                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item active>Transfert</Breadcrumb.Item>
+                </Breadcrumb>
+                <Row className="my-3">
+                    <Col md={4}>
                         <h3>Send Money</h3>
                     </Col>
-                    <Col xs={6}>
-                        <button type="button" className="btn btn-primary col-3">Add Connection</button>
+                    <Col md={{span: 4, offset:4}}>
+                        <button type="button" className="btn btn-primary ">Add Connection</button>
                     </Col>
                 </Row>
                     <Form noValidate validated={this.validated} onSubmit={this.handleSubmit}>
@@ -97,8 +101,8 @@ class Transfert extends React.Component{
                             <ToastContainer className="p-3" position={'top-center'}>
                             <Toast onClose={() => this.hideComponent("show1")} bg={'success'} show={show1} delay={3000} autohide>
                                     <Toast.Header>
-                                        <img src="holder.js/20x20?text=%20" className="rounded me-2"alt=""/>
-                                        <strong className="me-auto">Bootstrap</strong>
+                                        <img src={logo} width={120}  className="rounded me-2"alt=""/>
+                                        <strong className="me-auto"></strong>
                                         <small className="text-muted">just now</small>
                                     </Toast.Header>    
                                 <Toast.Body>Sauvegarde réussie !</Toast.Body>
@@ -107,36 +111,16 @@ class Transfert extends React.Component{
                             <ToastContainer className="p-3" position={'top-center'}>
                             <Toast onClose={() => this.hideComponent("show2")} bg={'danger'} show={show2} delay={3000} autohide>
                                     <Toast.Header>
-                                        <img src="holder.js/20x20?text=%20" className="rounded me-2"alt=""/>
-                                        <strong className="me-auto">Bootstrap</strong>
+                                        <img src={logo} width={120}  className="rounded me-2"alt=""/>
+                                        <strong className="me-auto"></strong>
                                         <small className="text-muted">just now</small>
                                     </Toast.Header>    
                                 <Toast.Body>Echec de sauvegarde !</Toast.Body>
                             </Toast>
                             </ToastContainer>
-
                         </Row>
                     </Form>
-                    
-                
-                <table className="table table-striped my-5 table-borderless">
-                    <thead>
-                        <tr className="table-success">
-                            <td>Connections</td>
-                            <td>Description</td>
-                            <td>Amount</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.transfert.map((trans)=>
-                        <tr key={`${trans.id}`}>
-                            <td>{`${trans.idCredit.name}`}</td>
-                            <td>{`${trans.description}`}</td>
-                            <td>{`${trans.amount}`}</td>
-                        </tr>
-                        )}
-                    </tbody>
-                </table>
+                    <Pagination/>
             </Container>
         );
     }
